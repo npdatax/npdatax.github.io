8c6d1da4ec407ad92c161ca7d94e8fa5674db9fc
vasuji <sigdeldkr@gmail.com>
2019-02-16 18:11:00 -0800

    updated
    updated

---
+++ Robotics - Doc/js/users.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+
+const router = express.Router();
+
+// Log a user out
+router.get("/logout", (req, res, next) => {
+  req.logout();
+  res.redirect("/");
+});
+
+
+module.exports = router;
+
+
+
